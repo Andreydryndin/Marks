cc8868450aafbc1c338bf3e0cbe7e21df2da6ad9
Andreydryndin <andrei18031997@yandex.ru>
2018-12-15 00:30:21 +0300

    Рефактор

--- index.php
+++ index.php
@@ -6,16 +6,16 @@ function pregChars($string){
 }
 
 function revertPunctuationMarks($string){
-    $marks = '!?.,:;(){}[]"\'-|<>';
-    $marks2 = '?!,.;:)(}{][\'"|-><';
-    $marks = pregChars($marks);
-    $marks2 = pregChars($marks2);
-    $Marks = array_combine($marks,$marks2);
+    $marks_initial = '!?.,:;(){}[]"\'-|<>';
+    $marks_replace = '?!,.;:)(}{][\'"|-><';
+    $marks_initial = pregChars($marks_initial);
+    $marks_replace = pregChars($marks_replace);
+    $Marks_initial = array_combine($marks_initial,$marks_replace);
     $string = pregChars($string);
 
     foreach ($string as $key => $value) {
-        if(array_search($value, $Marks) !== false){
-            $string[$key] = $Marks[$value];
+        if(array_search($value, $Marks_initial) !== false){
+            $string[$key] = $Marks_initial[$value];
         }
     }
     $string = implode('',$string);
@@ -23,5 +23,5 @@ function revertPunctuationMarks($string){
 }
 
 $result = revertPunctuationMarks("hi! how are you?");
-//echo $result;
+echo $result;
 
